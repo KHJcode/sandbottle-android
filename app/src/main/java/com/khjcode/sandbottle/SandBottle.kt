1a79c1a1c4a5b16a3654163512b3d998e29de9e2
KHJcode <kimstudswcode4485@gmail.com>
2021-10-15 11:58:14 +0900

    xml

---
+++ app/src/main/java/com/khjcode/sandbottle/SandBottle.kt
@@ -0,0 +1,11 @@
+package com.khjcode.sandbottle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SandBottle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
